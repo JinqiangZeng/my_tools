1d7be464f6c999becbcbf3b356a4e122a3da8610
Jinqiang <jinqiang.zeng@leiainc.com>
2016-04-08 12:02:27 -0700

    disolay function command name instead of function name

--- Gyro_Accel/timeStatistics.py
+++ Gyro_Accel/timeStatistics.py
@@ -4,6 +4,9 @@ def time_cal(func):
     def callf(*args, **kwargs):
         start = time()
         r = func(*args, **kwargs)
-        print("{} {}s".format(func.__name__ ,time() - start))
+        if len(args) > 0:
+            print("Time elapsed {} {}s".format(args[0],time() - start))
+        else:
+            print("Time elapsed {} {}s".format(func.__name__,time() - start))
         return r
     return callf
