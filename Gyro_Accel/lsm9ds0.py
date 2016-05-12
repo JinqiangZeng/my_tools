f7858ebee01513eab17d918a6fa071c59c6047a9
Jinqiang <jinqiang.zeng@leiainc.com>
2016-05-12 10:37:34 -0700

    Added 3d magnet plot and accelerometer delta z detection

--- Gyro_Accel/lsm9ds0.py
+++ Gyro_Accel/lsm9ds0.py
@@ -7,12 +7,15 @@ import json
 import matplotlib.pyplot as plt
 import os
 from timeStatistics import time_cal
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import cm
+import matplotlib.tri as mtri
 
 """
-Test cases for LSM9DS0, the 9 axies accelerometer/gyrocscope/magnetometer sensor
+Test cases for LSM9DS0, the 9 axies accelerometer/gyroscope/magnetometer sensor
 """
 
-serialfd = serial.Serial(serialPortDetect.serial_ports()[0], 4000000, timeout=1.5)
+serialfd = serial.Serial(serialPortDetect.serial_ports()[0], 4000000, timeout=2.5)
 
 TLEN = 1000
 #plot handler
@@ -67,10 +70,38 @@ def plotTwoArrays(px, py, figname):
     vmax, vmin = axisRange(py)
     hmax, hmin = axisRange(px)
 
+    ax = plt.figure().add_subplot(111)
     plt.axis([hmin, hmax, vmin, vmax])
-    plt.plot(px, py)
+    plt.grid(True)
+    ax.plot(px, py)
     plt.savefig(plotDir + '/' + figname)
     plt.cla()
+    plt.close()
+
+"""plot 3d magneto"""
+def plot3d(px, py, pz, figname):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    plt.grid(True)
+    ax.plot(px, py, pz, label='mag stero')
+    ax.legend()
+    plt.savefig(plotDir + '/' + figname)
+    plt.cla()
+    plt.grid(True)
+    ax.scatter(px, py, pz, zdir='z', label='mag stero')
+    ax.legend()
+    plt.savefig(plotDir + '/' + "s"+ figname)
+    plt.cla()
+    plt.close()
+
+def plotColor3d(x, y, z, figname):
+    triang = mtri.Triangulation(x, y)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_trisurf(triang,z, cmap=cm.coolwarm)
+    plt.show()
+    plt.cla()
+
 
 def plotArrays(s_type, duration):
     plotOnearray(xArray, duration, plotDir + '/' + s_type + '_x.png')
@@ -92,11 +123,16 @@ def sensorTest(cmd):
     duration = time() - start
     tArray = [i*duration/TLEN for i in range(TLEN)]
     plotArrays(cmd, duration)
+    if cmd == 'accel_get':
+        deltaz = []
+        deltaz.append(0)
+        deltaz.extend([abs(zArray[i+1]-zArray[i])  for i in range(0, TLEN-1) ])
+        plotOnearray(deltaz, duration, plotDir + '/' + '_deltaz.png')
 
 @time_cal
-def stest():
+def stest(cmd):
     for i in range(0,TLEN):
-        serialfd.write("sensors_get\r\n")
+        serialfd.write(cmd + "\r\n")
         serialfd.write("\r\n")
         sleep(0.01)
         str = serialfd.readline()
@@ -118,12 +154,34 @@ if __name__ == '__main__':
             gyro_cal()
         elif cmd == "acc":
             sensorTest('accel_get')
+        elif cmd == "finger":
+            stest('fingers_get 2')
         elif cmd == "exit":
             serialfd.close()
             sys.exit()
         elif cmd == "mag":
             sensorTest('mag_get')
             plotTwoArrays(xArray, yArray, "mag_xy.png")
+            print("center point max_min is ({}, {}, {})".format(sum(axisRange(xArray))/2.0, sum(axisRange(yArray))/2.0, sum(axisRange(zArray))/2.0))
+            print("center point average is ({}, {}, {})".format(sum(xArray)/len(xArray), sum(yArray)/len(yArray), sum(zArray)/len(zArray)))
+            print("max point is ({}, {}, {})".format(max(xArray), max(yArray), max(zArray)))
+            print("min point is ({}, {}, {})".format(min(xArray), min(yArray), min(zArray)))
+            print("radius point is ({}, {}, {})".format((max(xArray) - min(xArray))/2, (max(yArray) - min(yArray))/2, (max(zArray) - min(zArray))/2))
+            plot3d(xArray, yArray, zArray, "mag_xyz.png")
+            # plotColor3d(xArray, yArray, zArray, "mag_xyz.png")
+        elif cmd == "mag_cal":
+            serialfd.write("mag_cal 0 0 0 1 1 1\r\n")
+            serialfd.write("\r\n")
+            serialfd.write("\r\n")
+            print(serialfd.readline())
+            sensorTest('mag_get')
+            plotTwoArrays(xArray, yArray, "mag_cal_xy.png")
+            print("center point max_min is ({}, {}, {})".format(sum(axisRange(xArray))/2.0, sum(axisRange(yArray))/2.0, sum(axisRange(zArray))/2.0))
+            print("center point average is ({}, {}, {})".format(sum(xArray)/len(xArray), sum(yArray)/len(yArray), sum(zArray)/len(zArray)))
+            print("max point is ({}, {}, {})".format(max(xArray), max(yArray), max(zArray)))
+            print("min point is ({}, {}, {})".format(min(xArray), min(yArray), min(zArray)))
+            print("radius point is ({}, {}, {})".format((max(xArray) - min(xArray))/2, (max(yArray) - min(yArray))/2, (max(zArray) - min(zArray))/2))
+            plot3d(xArray, yArray, zArray, "mag_cal_xyz.png")
         elif cmd == "sensors":
-            stest()
+            stest("sensors_get")
 
